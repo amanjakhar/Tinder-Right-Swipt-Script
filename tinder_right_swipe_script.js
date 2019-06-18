3ba9432762e96ee993fe7e1b501c09479ba1322c
amanjakhar <amanjakharking@gmail.com>
2019-06-18 14:11:57 +0530

    Create tinder_right_swipe_script.js

---
+++ tinder_right_swipe_script.js
@@ -0,0 +1,5 @@
+a = setInterval(
+    function(){
+          var elem = document.getElementsByClassName("button Lts($ls-s) Z(0) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) Wc($transform) End(15px) Scale(1.1):h");
+          elem[0].click()
+          },2000)
